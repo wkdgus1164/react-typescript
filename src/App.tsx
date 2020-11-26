f075bdcbbda710e57cc9b4ef437cd850dd749778
Han Jang Hyun <wkdgus1164@naver.com>
2020-11-26 17:37:26 +0900

    initial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.scss';
+import {Typography} from "@material-ui/core";
+
+function App() {
+    return (
+        <Typography align="right" variant="h1" component="h2">h1. Heading!</Typography>
+    );
+}
+
+export default App;
---
+++ webpack.config.dev.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: {
+        main: './src/index.tsx'
+    },
+    output: {
+        filename: "[name].js",
+        path: path.resolve('./dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node-modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader'],
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html'
+        })
+    ],
+    devServer: {
+        host: 'localhost',
+        port: 3000,
+        open: true
+    }
+}
